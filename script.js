b9055c7c908aa0b4bb71797cc7b4e36483e29221
Mateus Reinaux <reinauxmateus@gmail.com>
2025-11-29 12:52:30 -0300

    Add click event to display thank you message

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnMensagem").addEventListener("click", function () {
+    const msg = document.getElementById("mensagem");
+    msg.textContent = "Obrigado por visitar meu portfólio! 🚀";
+});
